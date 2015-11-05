67d30226c69c13ccd7627b0b887779312e01cb10
Jon Acker <jacker@inviqa.com>
2015-11-05 09:41:58 +0000

    Don't use injectables service if none have been defined

---
+++ app/code/community/Inviqa/SymfonyContainer/Model/ServiceInjector.php
@@ -0,0 +1,52 @@
+<?php
+
+use Symfony\Component\DependencyInjection\Container;
+use Inviqa_SymfonyContainer_Model_InjectableCompilerPass as InjectableCompilerPass;
+
+class Inviqa_SymfonyContainer_Model_ServiceInjector
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * @param array $services
+     */
+    public function __construct(array $services)
+    {
+        $this->container = $services['container'];
+    }
+
+    /**
+     * @param $class
+     */
+    public function setupDependencies($class)
+    {
+        if (!$this->container->has(InjectableCompilerPass::INJECTABLES_SERVICE_ID)) {
+            return;
+        }
+
+        $references = $this->container->get(InjectableCompilerPass::INJECTABLES_SERVICE_ID)->references;
+
+        if (!isset($references[get_class($class)])) {
+            return;
+        }
+
+        if (!in_array('__dependencies', get_class_methods($class))) {
+            return;
+        }
+
+        $services = $this->getServices($references[get_class($class)]);
+
+        call_user_func_array([$class, '__dependencies'], $services);
+    }
+
+    /**
+     * @return array
+     */
+    private function getServices(array $serviceDefinitions)
+    {
+        return array_map(function($definition) { return $this->container->get($definition); }, $serviceDefinitions);
+    }
+}
